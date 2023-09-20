8ebdb0d6705c3684745ae62acd35fc8a98169d73
andmohiko <andmohiko@gmail.com>
2023-09-20 18:51:12 +0900

    feat: news page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    resolve: {
-      alias: {
-        '@components': '/src/components'
-      },
-    },
-  },
+  srcDir: 'src',
 });
